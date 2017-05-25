c7cbda7079304f26d453e212b22a763a6ff56553
Karen McCulloch <k.a.z.mcc95@gmail.com>
2017-05-25 23:04:09 +0100

    use the url to find the poll in the DB. use pug to show the poll nicely

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+var mongo_uri = process.env.MONGOLAB_URI;
+var collection = "voting-app";
+var mongo = require("mongodb").MongoClient;
+
+
+
+router.param("poll_id", function(req, res, next, id, name){
+    poll = {
+        slug: id,
+    }
+    mongo.connect(mongo_uri, function(err, db){
+        if (err) next(new Error("can't connect to mongo"));
+
+        col = db.collection(collection);
+        col.find(poll).toArray(function(err, docs){
+            db.close();
+            console.log(docs);
+            if (err) next(new Error("unable to query"));
+            req.poll = docs[0];
+            next();
+        })
+    })
+})
+
+
+router.route('/poll/:poll_id')
+    .get(function(req, res, next){
+        res.render("poll", req.poll)
+    })
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
