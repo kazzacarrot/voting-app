6e38fa8ea7d3b6b8e21e5edd507342d2d7c60b0d
Karen McCulloch <k.a.z.mcc95@gmail.com>
2017-05-26 20:57:15 +0100

    make it possible to pick one! This will update the db as if by magic

--- routes/index.js
+++ routes/index.js
@@ -27,19 +27,48 @@ router.param("poll_id", function(req, res, next, id, name){
 
 
 router.route('/poll/:poll_id')
-    .get(function(req, res, next){
-        res.render("poll", req.poll)
-    })
-    .post(function( req, res, next){
-        console.log(req.body);
+.get(function(req, res, next){
+    res.render("poll", req.poll)
+})
+.post(function( req, res, next){
+    console.log(req.body);
+    r = req.body;
+    answer = {
+        "text" : r["answer"]
+    }
+    if (answer.text === undefined){
+        res.render("error", {"message": "You have to pick one.", error: {status: "ain't no point if you don't pick one."}});
+    }
+    else{
+        console.log(answer);
+        mongo.connect(mongo_uri, function(err, db){
+            if (err) next(new Error("can't connect to mongo"));
 
-        res.render("done", req.poll)
-    })
+            query=    {
+                "_id":          req.poll._id,
+                "answers.text" : answer.text
+            }
+            console.log(query);
+            col = db.collection(collection);
+            col.findAndModify( query,
+                    [[    "_id", "asc" ]],
+                    {
+                        $inc: {"answers.$.count":1}
+                    },
+                    function(err, docs){
+                        if (err) console.error(err);
+                        console.log(docs);
+                        db.close(); 
+                        res.render("done", req.poll)
+                    })
+        })
+    }
+})
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
